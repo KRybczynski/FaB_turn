d6e83a77d4aa77c7885b004ddbc23435a19f836f
Krybczynski <krybczynski12@gmail.com>
2023-03-05 20:43:40 +0100

    few comments

--- fab_tura/fab_tura.cpp
+++ fab_tura/fab_tura.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include<chrono>
+#include <chrono>
 
 #include"card.h"
 #include"bruteForce.h"
@@ -24,31 +24,9 @@ void oneExample(int argc, char* argv[]) {
   std::vector<Card> hand;
   vector<string> g_biggest;
   int output;
-
-
   for (int i = 1; i < argc; i += 5) {
     hand.push_back(loadCard(argv, i));
   }
-
-  /*
-  hand.push_back(Card("Cl", 2, 0, 4, 0));  // added weapon
-  hand.push_back(Card("CnC01", 2, 1, 6, 0));
-  hand.push_back(Card("SSP03", 0, 3, 0, 3));
-  hand.push_back(Card("SS02", 1, 2, 6, 0));
-  hand.push_back(Card("BW02", 3, 2, 6, 0));
-  /*
-  for (auto a : hand) {
-    a.display();
-  }
-
-  hand.push_back(Card("cardNumber5", 6, 2, 2, 3));
-  hand.push_back(Card("cardNumber1", 3, 1, 9, 0));
-  hand.push_back(Card("cardNumber3", 2, 2, 12, 3));
-  hand.push_back(Card("cardNumber4", 6, 1, 11, 3));
-  hand.push_back(Card("cardNumber6", 8, 3, 3, 1));
-  hand.push_back(Card("cardNumber0", 3, 2, 1, 0));
-  hand.push_back(Card("cardNumber2", 3, 1, 11, 1));
-  */
   g_biggest.reserve(hand.size());
   auto start = std::chrono::high_resolution_clock::now();
   output = bruteForce(hand, 1, 0, hand.size(), g_biggest);
@@ -71,13 +49,11 @@ void oneExample(int argc, char* argv[]) {
   }
 }
 
-
+//output is only time needed to find the sequence
 void benchmark(int argc, char* argv[]) {
   std::vector<Card> hand;
   vector<string> g_biggest;
   int output = 0;
-
-
 
   for (int i = 1; i < argc; i += 5) {
     hand.push_back(loadCard(argv, i));
@@ -97,11 +73,10 @@ void benchmark(int argc, char* argv[]) {
   stop = std::chrono::high_resolution_clock::now();
   auto duration2 = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
   cout << duration2.count() << " ";
-
 }
 
-
+// ./fab_tura [card name] [card cost] [card pitch value] [card attack value] [card go_again] ...
 int main(int argc, char* argv[]) {
-  //oneExample( argc,  argv);
-  benchmark(argc, argv);
+  oneExample(argc,  argv);
+  //benchmark(argc, argv);
 }
